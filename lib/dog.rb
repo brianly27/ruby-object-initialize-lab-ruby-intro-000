c2c026ed2b9fd95bbcc9d59100072221267fe249
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-19 19:26:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 class Dog
-  def initialize(breed)
+  def initialize(name, breed)
+    @name = name
     @breed = breed
   end
 
   def breed=(breed)
     @breed = breed
+  end
+
+  def name=(name)
+    @name = name
   end
 
 end
